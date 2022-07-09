0fba67c0eefacee6244619b0127edb794657ebe5
Alexandre Bordiere <87599075+alexandre-bordiere@users.noreply.github.com>
2022-07-09 22:03:52 +0200

    add more aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,7 @@
+# Docker
+alias d="docker"
+alias dc="docker-compose"
+
 # Git
 alias gb="git branch"
 alias gc="git commit -m"
@@ -11,5 +15,6 @@ alias stash="git stash -u"
 alias wip="git add . && gc \"WIP\""
 
 # Utils
+alias reload="source ~/.zshrc"
 alias sshcopy="pbcopy < $HOME/.ssh/id_ed25519.pub"
 alias sshcreate="ssh-keygen -t ed25519 -C \"alexandre@bordiere.com\""
