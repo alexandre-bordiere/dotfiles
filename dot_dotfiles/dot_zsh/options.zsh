7987ca0314361d73b0e08ba649925514b7c4db55
Alexandre Bordiere <alexandrebordiere@gmail.com>
2025-03-28 22:56:27 +1100

    initial commit

---
+++ dot_dotfiles/dot_zsh/options.zsh
@@ -0,0 +1,15 @@
+setopt APPEND_HISTORY # adds history
+setopt COMPLETE_IN_WORD
+setopt EXTENDED_HISTORY # add timestamps to history
+setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_REDUCE_BLANKS
+setopt HIST_VERIFY
+setopt IGNORE_EOF
+setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+setopt LOCAL_OPTIONS # allow functions to have local options
+setopt LOCAL_TRAPS # allow functions to have local traps
+setopt NO_BG_NICE # don't nice background tasks
+setopt NO_HUP
+setopt NO_LIST_BEEP
+setopt PROMPT_SUBST
+setopt SHARE_HISTORY # share history between sessions
